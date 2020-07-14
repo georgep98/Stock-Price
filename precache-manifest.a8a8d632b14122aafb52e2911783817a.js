a1620ce127cea27a7692c5a0282d17c26ce71138
georgep98 <48506690+georgep98@users.noreply.github.com>
2020-07-14 16:09:43 +0300

    Updates

---
+++ precache-manifest.a8a8d632b14122aafb52e2911783817a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d442a3e8bec5aa963f26ca1f9d728792",
+    "url": "/Stock-Price/index.html"
+  },
+  {
+    "revision": "4c9123b4bb921195e9b6",
+    "url": "/Stock-Price/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "4dfd59b4e1ad7fac3ab3",
+    "url": "/Stock-Price/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4c9123b4bb921195e9b6",
+    "url": "/Stock-Price/static/js/2.7e0f2672.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/Stock-Price/static/js/2.7e0f2672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dfd59b4e1ad7fac3ab3",
+    "url": "/Stock-Price/static/js/main.1ee2e02d.chunk.js"
+  },
+  {
+    "revision": "b6cd4902f040246cffda",
+    "url": "/Stock-Price/static/js/runtime-main.d09d9e37.js"
+  }
+]);
